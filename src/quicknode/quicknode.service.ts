5e79edcad0af55bd59f9cb4dbe4485d570e5ceb4
Mihail Kirov <me@mikirov.xyz>
2024-11-26 23:57:52 +0200

    WIP

--- src/quicknode/quicknode.service.ts
+++ src/quicknode/quicknode.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  HttpException,
+  HttpStatus,
+  Logger,
+  BadRequestException,
+} from '@nestjs/common';
 import { QuicknodeEntity } from './entities/quicknode-provision.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -124,17 +130,25 @@ export class QuicknodeService {
   // TODO: check if quicknodeId is associated with RPC Plan or REST API Plan
   async deprovisionService(data: any): Promise<any> {
     this.logger.log(data);
-    const { 'quicknode-id': quicknodeId, plan } = data;
+    const { 'quicknode-id': quicknodeId } = data;
 
     const user = await this.userRepository.findOne({
       where: { quicknodeId },
     });
 
-    if (user) {
-      await this.paymentService.resetTier(user, plan);
-    } else {
-      return { status: 'error' };
+    const existingService = await this.quicknodeRepository.findOne({
+      where: { quicknodeId },
+    });
+
+    if (!existingService) {
+      throw new BadRequestException('Service not found');
     }
+
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    await this.paymentService.resetTier(user, existingService.plan);
 
     return {
       status: 'success',
